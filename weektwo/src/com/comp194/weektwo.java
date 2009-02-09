9d162b88978f62f3f651631d2bc7a80965a6c925
Ashish Datta <ashish@setfive.com>
2009-02-09 16:48:32 -0500

    Added week two's project

---
+++ weektwo/src/com/comp194/weektwo.java
@@ -0,0 +1,13 @@
+package com.comp194;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class weektwo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
